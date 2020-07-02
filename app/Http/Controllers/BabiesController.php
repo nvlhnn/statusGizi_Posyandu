eca1dd06b7d4775eba77840ace633d8461928233
Nvlhnn <hanannaufal17@gmail.com>
2020-07-02 16:02:17 +0700

    update bayi

--- app/Http/Controllers/BabiesController.php
+++ app/Http/Controllers/BabiesController.php
@@ -71,5 +71,70 @@ class BabiesController extends Controller
 
     }
 
+
+    public function edit($id)
+    {
+        $baby = Baby::find($id);
+        return view('Data Anak/edit', compact('baby'));    
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $messages = [
+            'required' => ':attribute wajib diisi',
+            'alpha' => ':attribute harus berupa huruf saja',
+            'numeric' => ':attribute harus berupa angka saja',
+        ];
+
+        $attributes=[
+            'name' =>'Nama Bayi',
+            'gender' => 'Jenis Kelamin',
+            'born' => 'Usia',
+            'height' => 'Tinggi Badan',
+            'weight' => 'Berat Badan',
+            'dad' => 'Nama Ayah',
+            'mom' => 'Nama Ibu' ,
+            'address' => 'Alamat'                        
+
+        ];
+
+        $this->validate($request,[
+            'name' => 'required|alpha',
+            'gender' => 'required',
+            'born' => 'required',
+            'height' => 'required|numeric',
+            'weight' => 'required|numeric',
+            'dad' => 'required|alpha',
+            'mom' => 'required|alpha', 
+            'address' =>'required'
+
+        ], $messages, $attributes);
+
+        $baby = Baby::find($id);
+        $baby->name  = $request->name;
+        $baby->gender  = $request->gender;
+        $baby->born  = $request->born;
+        $baby->height = $request->height;
+        $baby->weight = $request->weight;
+        $baby->dad   = $request->dad;
+        $baby->mom = $request->mom;
+        $baby->address      = $request->address;
+
+        
+        $baby->save();
+        session()->flash('notif', 'Destinasi Berhasil diubah');
+
+        return redirect()->route('babies.index'); 
+    }
+
+    public function destroy($id)
+    {
+        $place = Baby::find($id);
+        $place->delete();
+
+        return redirect()->route('babies.index');
+    }
+
     
 }
--- resources/views/Data Anak/edit.blade.php
+++ resources/views/Data Anak/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
    <!-- Remove This Before You Start -->
-   <h1>Ubah Destinasi Wisata</h1>
+   <h1>Tambah Data Anak</h1>
    <hr>
    @if (count($errors) > 0)
    <div class="alert alert-danger">
@@ -14,41 +14,55 @@
        </ul>
    </div>
    @endif
-   <form action="{{ route('adminPlaces.update', $place->id) }}" method="post" enctype="multipart/form-data">
-       {{ csrf_field() }}
-       @method('PUT')
-
+   <form action="{{ route('babies.update', $baby->id) }}" method="post" enctype="multipart/form-data">
+    {{ csrf_field() }}
+    @method('PUT')
        <div class="form-group">
-           <label for="name">Nama Destinasi:</label>
-           <input type="text" class="form-control" id="" name="name" value="{{$place->nama}}">
+           <label for="name">Nama Bayi:</label>
+           <input type="text" class="form-control" id="" name="name" value="{{$baby->name}}">
        </div>
-       <div class="form-group ">
-            <label for="day">Hari Buka: </label>
-            <input type="text" class="form-control" id="" name="day" placeholder="contoh : senin-rabu & jumat-minggu" value="{{$place->hari}}">
-        </div>
-       <div class="form-group">
-           <label for="open">Jam Buka: </label>
-           <input type="time" class="form-control" id="" name="open" value="{{$place->buka}}">
-       </div>
-       <div class="form-group">
-           <label for="close">Jam Tutup: </label>
-           <input type="time" class="form-control" id="" name="close" value="{{$place->tutup}}">
-       </div>
-       <div class="form-group">
-           <label for="price">Harga Tiket:</label>
-           <input type="text" class="form-control" id="" name="price" value="{{$place->harga_tiket}}">
-       </div>
-       <div class="form-group">
-            <label for="facl">Fasilitas:</label>
-            <input type="text" class="form-control" id="" name="facl" value="{{$place->tempat_umum}}">
-        </div>
-       <div class="form-group">
-            <label for="desc">Deskripsi:</label>
-            <textarea class="form-control" id="alamat" name="desc">{{$place->deskripsi}}</textarea>
+        <fieldset class="form-group">
+            <div class="row">
+              <legend class="col-form-label col-sm-2 pt-0">Jenis Kelamin</legend>
+              <div class="col-sm-10">
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="gender" id="gridRadios1" value="Laki-Laki" >
+                  <label class="form-check-label" for="gridRadios1">
+                    Laki-Laki
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="gender" id="gridRadios2" value="Perempuan">
+                  <label class="form-check-label" for="gridRadios2">
+                    Perempuan
+                  </label>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+        <div class="form-group">
+            <label for="name">Tanggal Lahir:</label>
+            <input type="date" class="form-control" id="" name="born" value="{{$baby->born}}">
         </div>
         <div class="form-group">
-            <label for="gambar">Masukkan File Gambar</label>
-            <input type="file" name="gambar">
+            <label for="name">Tinggi Badan:</label>
+            <input type="text" class="form-control" id="" name="height" value="{{$baby->height}}">
+        </div>                
+       <div class="form-group">
+            <label for="day">Berat Badan: </label>
+            <input type="text" class="form-control" id="" name="weight" value="{{$baby->weight}}">
+        </div>
+       <div class="form-group">
+           <label for="open">Nama Ayah: </label>
+           <input type="text" class="form-control" id="" name="dad" value="{{$baby->dad}}">
+       </div>
+       <div class="form-group">
+           <label for="close">Nama Ibu: </label>
+           <input type="text" class="form-control" id="" name="mom" value="{{$baby->mom}}">
+       </div>
+       <div class="form-group">
+            <label for="desc">Alamat:</label>
+            <textarea class="form-control" id="alamat" name="address" value="">{{$baby->address}}</textarea>
         </div>
        <div class="form-group">
            <button type="submit" class="btn btn-md btn-primary">Submit</button>
